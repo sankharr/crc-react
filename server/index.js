dba04385f8b7f6521280af8c14f148500034bc0a
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2022-04-04 08:17:55 +0530

    .env configured + connecting to the database done

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  })
+  .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
